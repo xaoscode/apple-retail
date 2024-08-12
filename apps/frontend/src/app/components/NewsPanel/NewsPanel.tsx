422a5cc2993350e731dab3c3259547335364d5bd
Buryak Nikita <laimongrace@gmail.com>
2024-08-13 03:18:36 +1000

    Add buttons on NewsPanel

--- apps/frontend/src/app/components/NewsPanel/NewsPanel.tsx
+++ apps/frontend/src/app/components/NewsPanel/NewsPanel.tsx
@@ -1,8 +1,11 @@
 import { NewsPanelProps } from "./NewsPanel.props";
 import styles from "./NewsPanel.module.css";
+import { Button } from "@/components/Button/Button";
 
 export const NewsPanel = ({ ...props }: NewsPanelProps): JSX.Element => {
     return <div className={ styles['news'] }  { ...props }>
+        <Button icon="/right-arrow.svg" className={ styles['right-button'] } size="medium" design="borderless"></Button>
+        <Button icon="/left-arrow.svg" className={ styles['left-button'] } size="medium" design="borderless"></Button>
 
     </div>
 }
